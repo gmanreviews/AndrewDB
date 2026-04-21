dfad45f29cf3f69e1a2fbc0ee34744ac1264c430
Andrew Robinson <andrewneilrobinson@gmail.com>
2026-04-20 21:28:07 -0400

    some random code
    I truly don't know rust and trying to random things

---
+++ andrew_db_proj/src/lexer.rs
@@ -0,0 +1,24 @@
+﻿pub struct Lexer
+{
+    //identifiers: Vec<Identifier>,
+}
+
+impl Lexer
+{
+    pub fn new() -> Lexer
+    {
+        //to figure this out one day
+        Lexer { } //identifiers: vec![] }
+    }
+
+    pub fn parse_text(text: &str) -> Lexer
+    {
+        //time to parse this stuff
+
+        println!("Parsing text... {text}");
+        let lexer = Lexer::new();
+        return lexer;
+    }
+
+
+}
--- andrew_db_proj/src/main.rs
+++ andrew_db_proj/src/main.rs
@@ -1,3 +1,14 @@
+//mod lexer_impl;
+//use lexer::
+
+
+mod lexer;
+
 fn main() {
+
+    let test = "SELECT COL FROM MAIN";
+    //let lexer = lexer::new();
+    _ = lexer::Lexer::parse_text(test);
+
     println!("Hello, world!");
 }
